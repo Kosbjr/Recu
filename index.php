daf34f8fb60d42edc72dd41488dc82938790d1f8
Héctor <hectorgarcia05.hgg@gmail.com>
2026-05-27 20:33:12 +0200

    forzar error

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 
 <div class="container">
 
-    <h1 id="saludo"><?php echo $saludo; ?></h1>
+    <h1 id="saluffdo"><?php echo $saludo; ?></h1>
 
     <p>El saludo lo genera PHP y el botón lo controla JS</p>
 
